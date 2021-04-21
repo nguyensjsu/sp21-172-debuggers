a7dcff16e9cf6687a85ea9ff0c120629e9230385
jashan6 <35910063+jashan6@users.noreply.github.com>
2021-04-21 14:24:27 -0700

    cashiers app

---
+++ starbucks_app/src/main/java/com/example/starbucks_app/StarbucksAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.starbucks_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksAppApplication.class, args);
+	}
+
+}
---
+++ starbucks_app/src/test/java/com/example/starbucks_app/StarbucksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starbucks_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
