85a80f6c14b5f734e23e8452a22cee662e2dacf2
briellep <brielle.purnell@sjsu.edu>
2021-04-21 16:56:19 -0700

    Spring Initializer

---
+++ starbucks-group-project/src/main/java/com/example/starbucksgroupproject/StarbucksGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.starbucksgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksGroupProjectApplication.class, args);
+	}
+
+}
---
+++ starbucks-group-project/src/test/java/com/example/starbucksgroupproject/StarbucksGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starbucksgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
