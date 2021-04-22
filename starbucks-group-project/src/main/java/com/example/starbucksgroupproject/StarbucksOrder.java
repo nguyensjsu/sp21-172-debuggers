ebe1bdb83b79a868e47aac115bffe87bbe24cff8
briellep <brielle.purnell@sjsu.edu>
2021-04-21 17:16:16 -0700

    Create StarbucksOrder.java

---
+++ starbucks-group-project/src/main/java/com/example/starbucksgroupproject/StarbucksOrder.java
@@ -0,0 +1,32 @@
+package com.example.starbucksgroupproject;
+
+import java.util.Objects;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+import jdk.jfr.DataAmount;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "ORDERS")
+@Data
+@RequiredArgsConstructor
+
+class StarbucksOrder {
+
+    private @Id @GeneratedValue Long id;
+
+    @Column(nullable=false)     private String drink;
+    @Column(nullable=false)     private String milk;
+    @Column(nullable=false)     private boolean activated;
+    @Column(nullable=false)     private double total;
+                                private String status;
+}
