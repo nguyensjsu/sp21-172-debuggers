2c00d5f369eebedb0dc8f03dae67d10141d51bfb
jashan6 <35910063+jashan6@users.noreply.github.com>
2021-05-18 17:46:23 -0700

    remove not needed files

--- starbucks_app/src/main/java/com/example/starbucks/StarbucksOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.starbucks;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface StarbucksOrderRepository extends JpaRepository<StarbucksOrder, Long> {
-    
-}
--- starbucks_app/src/main/java/com/example/starbucks/cashiers/CashiersRepository.java
+++
@@ -1,8 +0,0 @@
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface CashiersdRepository extends JpaRepository<CashiersCommand, Long> 
-{
-	
-}
